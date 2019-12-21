e51ff3a6b86623407a028079aa6bdbc0587e3b3a
Bosgnoom <paul.schouten@gmail.com>
2019-12-21 16:29:09 +0100

    Added logging

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ Purpose: read water metering gauge in my electrical cabinet
 A raspberry pi camera is mounted in a BrightPi LED enclosure. 
 
 Steps to be taken:
-* Switch on LEDs (BrightPi) [x]
-* Take picture (openCV) [x]
-    * Find circle contours (Hough transform) []
-    * Find rectangular contour (see credit card OCR) []
-    * Rotate to level []
-    * Cut out each digit []
-* Analyse each digit []
-    * Apply kkN nearest neighbour []
-* Compile into one number []
-* Send number to []
-    * Domoticz []
-    * InfluxDB []
+* [x] Switch on LEDs (BrightPi) 
+* [x] Take picture (openCV) 
+    * [] Find circle contours (Hough transform) 
+    * [] Find rectangular contour (see credit card OCR) 
+    * [] Rotate to level 
+    * [] Cut out each digit 
+* [] Analyse each digit 
+    * [] Apply kkN nearest neighbour 
+* [] Compile into one number 
+* [] Send number to 
+    * [] Domoticz 
+    * [] InfluxDB 
 
 ## Switch on LEDs
 The [BrightPi](https://uk.pi-supply.com/products/bright-pi-bright-white-ir-camera-light-raspberry-pi) uses a Semtech SC620 LED driver. It is connected to the Raspberry Pi using I2C.
--- watermeter.py
+++ watermeter.py
@@ -3,17 +3,24 @@
 import smbus
 import time
 import cv2
+import logging
 
+
+# Start logger
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 ###[ LED control
 def configure_leds():
     i2c_bus = smbus.SMBus(1)
 
-    # Set the dimming control for all leds to 40, 40x.28125=11.25 mA for each LED
+    # Set the dimming control for all leds to 40, 
+    # 40x.28125=11.25 mA for each LED
+    logging.debug("Set LED dimming control")
     for i in range(1, 8):
         i2c_bus.write_byte_data(0x70, i, 40)
 
     # Set the gain control to 0x08h (default, 281.25 micro-ampere)
+    logging.debug("Set LED gain control")
     i2c_bus.write_byte_data(0x70, 0x09, 0x08)
 
 
@@ -21,7 +28,7 @@ def leds_on():
     i2c_bus = smbus.SMBus(1)
 
     #Switch all (outer) LEDs on
-    print("On")
+    logging.info("Switching LEDs on")
     i2c_bus.write_byte_data(0x70, 0x00, 0xFF)
 
 
@@ -29,26 +36,36 @@ def leds_off():
     i2c_bus = smbus.SMBus(1)
 
     #Switchs all LEDs off
-    print("Off")
+    logging.info("Switching LEDs off")
     i2c_bus.write_byte_data(0x70, 0x00, 0x00)
 
 
+###[ Image processing
 def capture_image():
+    logging.info("Capturing image")
     cap = cv2.VideoCapture(0)
 
     if cap.isOpened():
         ret, frame = cap.read()
         if ret:
-            print("Frame was read")
+            logging.debug("Frame was read")
 
 
+def main():
+    logging.debug("Starting main loop")
+    configure_leds()
+    leds_on()
 
-configure_leds()
-leds_on()
+    capture_image()
 
-capture_image()
+    leds_off()
+    
+    logging.debug("Main loop finished")
+    
 
-leds_off()
+if __name__ == '__main__':
+    main()
+    
 
 
 
