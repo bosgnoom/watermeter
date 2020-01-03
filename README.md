9a5b61f5a71150e98a86264647b226b5aed54b62
Bosgnoom <paul.schouten@gmail.com>
2020-01-03 21:00:47 +0100

    Detect meter location by using HoughCircles, started to detect needed rotation angle using HoughLines

--- README.md
+++ README.md
@@ -9,16 +9,16 @@ Steps to be taken:
 * [x] Switch on LEDs (BrightPi) 
 * [x] Take picture (~~openCV~~ PiCamera) 
 * [ ] Analyse picture
-    * [] Find circle contours (Hough transform) 
-    * [] Find rectangular contour (see credit card OCR) 
-    * [] Rotate to level 
-    * [] Cut out each digit 
-* [] Analyse each digit 
-    * [] Apply kkN nearest neighbour 
-* [] Compile into one number 
-* [] Send number to 
-    * [] Domoticz 
-    * [] InfluxDB 
+    * [ ] Find circle contours (Hough transform) 
+    * [ ] Find rectangular contour (see credit card OCR) 
+    * [ ] Rotate to level 
+    * [ ] Cut out each digit 
+* [ ] Analyse each digit 
+    * [ ] Apply kkN nearest neighbour 
+* [ ] Compile into one number 
+* [ ] Send number to 
+    * [ ] Domoticz 
+    * [ ] InfluxDB 
 
 ## Switch on LEDs
 The [BrightPi](https://uk.pi-supply.com/products/bright-pi-bright-white-ir-camera-light-raspberry-pi) uses a Semtech SC620 LED driver. It is connected to the Raspberry Pi using I2C.
@@ -26,5 +26,10 @@ The (7-bit) I2C address is 0x70. First, set the gain and dimmer control. Switchi
 
 ## Using the pi-camera from pyton
 - First attempt using OpenCV. It seems it is hard to set the exposure time to "night" or something like this, so
-- Second attempt is using python3-picamera
+- Second attempt is using python3-picamera, works so far...
+
+## Performance
+This python (script?) is running on a Raspberry Pi 1 (yes!). Importing the python's libraries (modules)
+takes a while, so I am attempting to reduce the import load by switching from `import cv` to `from cv import ...`
+
 
--- watermeter.py
+++ watermeter.py
@@ -2,16 +2,18 @@
 
 import smbus
 import time
-import cv2
 import logging
 import picamera
 import numpy as np
 
+# Uitproberen minder importeren
+import cv2
+#from cv2 import imwrite, cvtColor, medianBlur, HoughCircles, circle
 
 # Start logger
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-###[ LED control
+###[ LED control ]############################################################
 def configure_leds():
     i2c_bus = smbus.SMBus(1)
 
@@ -42,7 +44,7 @@ def leds_off():
     i2c_bus.write_byte_data(0x70, 0x00, 0x00)
 
 
-###[ Acquire image
+###[ Acquire image ]##########################################################
 def capture_image():
     try:
         with picamera.PiCamera() as camera:
@@ -52,17 +54,82 @@ def capture_image():
             
             camera.start_preview()
             
-            time.sleep(2)
+            time.sleep(5)
             
             logging.info("Capturing image")
             image = np.empty((2464, 3296, 3), dtype=np.uint8)
             camera.capture(image, 'bgr')
             
-            logging.info("Writing image")
-            cv2.imwrite("/var/www/html/full.jpg", image)
     except:
         logging.error("Camera not available")
         quit(-1)
+
+    #logging.info("Writing full image")
+    #cv2.imwrite("/var/www/html/watermeter/full.png", image)
+            
+    logging.debug("Convert to gray")
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #cv2.imwrite("/var/www/html/watermeter/gray.png", gray)
+    return gray
+
+###[ Find circles ]################################
+def find_circle(image):
+    logging.info("Looking for circles")
+    logging.debug("Blur image")
+    #image = cv2.resize(image, (800, 600), interpolation=cv2.INTER_NEAREST)
+
+    img = cv2.medianBlur(image, 5)
+    cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    
+    logging.debug("Running HoughCircles")
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, 
+        param1=75, param2=200, minRadius=100, maxRadius=300)
+    print(circles)
+    circles = np.uint16(np.around(circles))
+    
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),1)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    
+    logging.debug("Writing image")    
+    cv2.imwrite("/var/www/html/watermeter/circles.png", cimg)
+
+    logging.debug("Writeing cropped image...")
+    circle = circles[0, 0]
+    x = circle[0]
+    y = circle[1]
+    r = circle[2]
+    roi = cimg[y-r:y+r, x-r:x+r]
+    cv2.imwrite("/var/www/html/watermeter/crop.png", roi)
+    
+    return img[y-r:y+r, x-r:x+r]
+
+
+def rotate_image(img):
+    logging.info("Rotating image")
+    
+    logging.debug("Edges")
+    edges = cv2.Canny(img, 50, 100)
+    cv2.imwrite('/var/www/html/watermeter/edges.png', edges)
+    
+    logging.debug("HoughLines")
+    lines = cv2.HoughLines(edges, 1, np.pi/180, 100)
+    print("Lines: {}".format(lines))
+    for rho,theta in lines[0]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+    cv2.imwrite('/var/www/html/watermeter/lines.png',img)
 
 
 def main():
@@ -70,9 +137,14 @@ def main():
     configure_leds()
     leds_on()
 
-    capture_image()
-
+    full_image = capture_image()
+    
     leds_off()
+
+    circle = find_circle(full_image)
+    rotate_image(circle)
+
+
     
     logging.debug("Main loop finished")
     
